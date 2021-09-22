7f85e3cfd2724391d34bdd0df37dd7515cb60ddb
branko-stankovic <branko.stankovic@gmail.com>
2021-09-22 23:59:52 +0200

    Game machine completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+let playerScore = 0;
+let computerScore = 0;
+
+let rounds = 5;
+
 // random choice generator for AI
 let computerPlay = function() {
     let moves = ["Rock", "Paper", "Scissors"];
@@ -15,36 +20,71 @@ let playRound = function(playerSelection, computerSelection) {
 
     // handle all the possible outcomes
     if (player == "rock") {
+
         // player chooses rock
         if (computer == "rock") {
             return "It's a tie! Both chose rock!";
         } else if (computer == "paper") {
+            computerScore++;
             return "Computer wins! Paper beats rock!";
         } else if (computer == "scissors") {
+            playerScore++;
             return "Player wins! Rock beats scissors!";
         }
     } else if (player == "paper") {
+
         // player chooses paper
         if (computer == "rock") {
+            playerScore++;
             return "Player wins! Paper beats rock!";
         } else if (computer == "paper") {
             return "It's a tie! Both chose paper!";
         } else if (computer == "scissors") {
+            computerScore++;
             return "Computer wins! Scissors beats paper!";
         }
     } else if (player == "scissors") {
+
         // player chooses scissors
         if (computer == "rock") {
+            computerScore++;
             return "Computer wins! Rock beats scissors!";
         } else if (computer == "paper") {
+            playerScore++;
             return "Player wins! Scissors beats paper!";
         } else if (computer == "scissors") {
             return "It's a tie! Both chose scissors";
         }
+    } else {
+        return "Invalid choice! Please choose type 'Rock', 'Paper' or 'Scissors'";
     }
 }
 
-const playerSelection = "ROCK";
-const computerSelection = computerPlay();
+// game machine
+let game = function() {
 
-console.log(playRound(playerSelection, computerSelection));
+    for (let i = 0; i < rounds; i++) {
+
+        // make computer choose again, then take input from player
+        let computerSelection = computerPlay();
+        let playerSelection = window.prompt("Rock, Paper or Scissors?");
+
+        // play a single round
+        console.log(playRound(playerSelection, computerSelection));
+
+        console.log("player: " + playerScore);
+        console.log("computer: " + computerScore);
+
+    }
+
+    // announce the winner!!! (and the fate of humanity)
+    if (playerScore > computerScore) {
+        return "Player wins!!! Go go humans!";
+    } else if (computerScore > playerScore) {
+        return "AI wins! Run for your lives!!!";
+    } else {
+        return "It's a tie! There is still hope for humanity.";
+    }
+}
+
+console.log(game());
